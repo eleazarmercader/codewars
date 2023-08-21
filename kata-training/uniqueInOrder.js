335e389fadb3ace0c9792f92908f3189b452f4c1
eleazarmercader <elmercaderdesundberg@gmail.com>
2023-08-21 15:45:02 +0200

    adding sample test to uniqueInOrder

--- kata-training/uniqueInOrder.js
+++ kata-training/uniqueInOrder.js
@@ -22,3 +22,15 @@ var uniqueInOrder = function(iterable) {
   }
 
   return result;
+
+/* SAMPLE TEST
+ * const chai = require("chai");
+ * const assert = chai.assert;
+ * chai.config.truncateThreshold=0;
+ * 
+ * describe("Tests", () => {
+ * 	it("test", () => {
+ * 		assert.deepEqual(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
+ * 	});
+ * });
+*/
